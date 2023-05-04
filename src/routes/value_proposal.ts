a8e8d9b9610595633af9ed819ba462868dd102c8
Jose Alejandro Moreno Agudelo <jose.moreno@mercadolibre.com.co>
2023-05-03 23:16:30 -0500

    fixing

--- src/routes/value_proposal.ts
+++ src/routes/value_proposal.ts
@@ -2,6 +2,6 @@ import express from 'express';
 import {getValueProposal} from "../controller/value_proposal"
 const router = express.Router();
 
-router.get("/sites/MLB/v1/value-proposal/6", getValueProposal);
+router.get("/sites/MLB/v1/value-proposal/3", getValueProposal);
 
 export {router}
